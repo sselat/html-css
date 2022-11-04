505605eb87d4d72d1d2b61f35c30a72ce1f85176
sselat <tales.beraldo@gmail.com>
2022-11-04 16:49:20 -0400

    aprendendo sobre components

---
+++ aulas-vueJS/projeto_vue/src/components/Info.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <p>Estou trabalhando no momento.</p>
+        <p>Utilizo as seguintes tecnologias:</p>
+        <ul>
+            <li>HTML</li>
+            <li>CSS</li>
+            <li>JavaScript</li>
+        </ul>
+    </div>
+</template>
+<script>
+export default {
+    name: 'Info'
+}
+</script>
